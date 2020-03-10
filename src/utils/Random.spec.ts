5616cab49dfb0c81af9e3156405be6b9021f342e
Tobias Munzert <tobias.munzert@gmail.com>
2020-03-10 07:58:32 +0100

    extended random with seed functionality

--- src/utils/Random.spec.ts
+++ src/utils/Random.spec.ts
@@ -1,22 +1,71 @@
-import { randomInt } from "./Random";
+import { randomInt, random } from "./Random";
 
 describe('randomInt', () => {
   test('0 to 1 with no seeed', () => {
-    const result = randomInt(0, 1);
-    expect(result).toBeGreaterThanOrEqual(0);
-    expect(result).toBeLessThanOrEqual(1);
+    for (let i = 0; i < 100; i++) {
+      const result = randomInt(0, 1);
+      expect(Math.abs(result % 1)).toBe(0);
+      expect(result).toBeGreaterThanOrEqual(0);
+      expect(result).toBeLessThanOrEqual(1);
+    }
   });
   test('-100 to 100 with no seeed', () => {
-    const result = randomInt(-100, 100);
-    expect(result).toBeGreaterThanOrEqual(-100);
-    expect(result).toBeLessThanOrEqual(100);
+    for (let i = 0; i < 100; i++) {
+      const result = randomInt(-100, 100);
+      expect(Math.abs(result % 1)).toBe(0);
+      expect(result).toBeGreaterThanOrEqual(-100);
+      expect(result).toBeLessThanOrEqual(100);
+    }
   });
   test('0 to 1 with seeed', () => {
-    const result = randomInt(0, 1);
-    expect(result).toBe(0);
+    const result = randomInt(0, 1, 42);
+    expect(Math.abs(result % 1)).toBe(0);
+    expect(result).toBeGreaterThanOrEqual(0);
+    expect(result).toBeLessThanOrEqual(1);
+    for (let i = 0; i < 100; i++) {
+      expect(result).toBe(randomInt(0, 1, 42));
+    }
   });
   test('-100 to 100 with seeed', () => {
     const result = randomInt(-100, 100, 42);
-    expect(result).toBe(87);
+    expect(Math.abs(result % 1)).toBe(0);
+    expect(result).toBeGreaterThanOrEqual(-100);
+    expect(result).toBeLessThanOrEqual(100);
+    for (let i = 0; i < 100; i++) {
+      expect(result).toBe(randomInt(-100, 100, 42));
+    }
+  });
+});
+
+describe('random', () => {
+  test('0 to 1 with no seeed', () => {
+    for (let i = 0; i < 100; i++) {
+      const result = random(0, 1);
+      expect(result).toBeGreaterThanOrEqual(0);
+      expect(result).toBeLessThanOrEqual(1);
+    }
+  });
+  test('-100 to 100 with no seeed', () => {
+    for (let i = 0; i < 100; i++) {
+      const result = random(-100, 100);
+      expect(result).toBeGreaterThanOrEqual(-100);
+      expect(result).toBeLessThanOrEqual(100);
+    }
+  });
+  test('0 to 1 with seeed', () => {
+    const result = random(0, 1, 42);
+    expect(result).toBeGreaterThanOrEqual(0);
+    expect(result).toBeLessThanOrEqual(1);
+    for (let i = 0; i < 100; i++) {
+      expect(result).toBe(random(0, 1, 42));
+    }
+  });
+  test('-100 to 100 with seeed', () => {
+    const result = random(-100, 100, 42);
+    expect(result).toBeGreaterThanOrEqual(-100);
+    expect(result).toBeLessThanOrEqual(100);
+    for (let i = 0; i < 100; i++) {
+      expect(result).toBe(random(-100, 100, 42));
+    }
   });
 });
--- src/utils/Random.ts
+++ src/utils/Random.ts
@@ -1,9 +1,13 @@
 export const randomInt = (from: number = 0, to: number = 100, seed?: number): number => {
+  return Math.floor(random(Math.ceil(from), Math.floor(to) + 1, seed));
+}
+
+export const random = (min: number = 0, max: number = 1, seed?: number): number => {
+  let n: number; // [0, 1[
   if (typeof seed !== "undefined") {
-    const mask = Number.MAX_SAFE_INTEGER;
-    const a = (Math.sin(seed + 1337) * mask) & mask;
-    return Math.abs(a) % (to - from + 1) + from;
+    n = Math.abs(Math.sin(seed + 5)) % 1;
   } else {
-    return Math.floor(Math.random() * (to - from + 1)) + from;
+    n = Math.random();
   }
+  return n * (max - min) + min;
 }
