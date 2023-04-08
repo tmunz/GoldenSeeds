ea2fd37f667038c0745b5ed598b46c240b5de4a5
Tobias Munzert <tobias.munzert@gmail.com>
2023-04-08 10:47:42 +0200

    improved text drawer

--- src/domain/generator/text/TextDrawer.ts
+++ src/domain/generator/text/TextDrawer.ts
@@ -7,14 +7,18 @@ export interface TextDrawerConfig {
   },
   text: {
     content: string;
-    size: number;
+    size: (n: number, items: number) => number;
   },
 }
 
 export function draw(config: TextDrawerConfig, grid: Point[]): { svg: string; points: Point[] } {
   return grid.reduce(
-    (agg, p) => {
+    (agg, p, n) => {
       const svg = `<text
+        letter-spacing="0"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        font-size="${config.text.size(n, grid.length)}"
         x="${p[Point.X]}"
         y="${p[Point.Y]}"
         fill="${config.style.color.toRgbHex()}"
